b2fc5a58c1d67a242998df6ccf7bf140f31431ea
mediakit4u <mediakit4u@gmail.com>
2025-03-21 01:28:19 +0530

    Update database.py

--- digital-business-card/backend/database.py
+++ digital-business-card/backend/database.py
@@ -1,1 +1,9 @@
+import json
 
+def save_card(data):
+    with open("cards.json", "a") as file:
+        file.write(json.dumps(data) + "\n")
+
+def get_cards():
+    with open("cards.json", "r") as file:
+        return [json.loads(line) for line in file.readlines()]
